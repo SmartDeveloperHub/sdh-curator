73eb1a2cf8e6b1540fadf4465a08617fca4c3949
fserena <fernando.serena@centeropenmiddleware.com>
2015-10-14 18:11:05 +0200

    Extracting delivery channel info from message

--- sdh/curator/actions/enrichment.py
+++ sdh/curator/actions/enrichment.py
@@ -31,19 +31,59 @@ __author__ = 'Fernando Serena'
 
 log = logging.getLogger('sdh.curator.actions.enrichment')
 CURATOR = Namespace('http://www.smartdeveloperhub.org/vocabulary/curator#')
-
+AMQP = Namespace('http://www.smartdeveloperhub.org/vocabulary/amqp#')
 
 class EnrichmentAction(Action):
     def run(self):
-        log.info('Received a potential enrichment request')
-        request_nodes = list(self._request_graph.subjects(RDF.type, CURATOR.EnrichmentRequest))
-        if not (0 < len(request_nodes) < 2):
-            raise SyntaxError('There is no enrichment request specified in the message')
+        log.debug('Received a potential enrichment request')
+        try:
+            request_node = self._request_graph.subjects(RDF.type, CURATOR.EnrichmentRequest, card=1)
+        except ValueError:
+            raise SyntaxError('There is not an only enrichment request specified in the message')
 
-        request_node = request_nodes.pop()
-        target_resource_nodes = list(self._request_graph.objects(request_node, CURATOR.targetResource))
-        if not (0 < len(target_resource_nodes) < 2):
-            raise SyntaxError('Invalid number of target resources: {}'.format(len(target_resource_nodes)))
-
-        target_resource = target_resource_nodes.pop()
+        try:
+            target_resource = self._request_graph.objects(request_node, CURATOR.targetResource, card=1)
+        except ValueError as e:
+            raise SyntaxError('Invalid number of target resources: {}'.format(e.message))
         log.debug('Found a request to enrich {}!'.format(target_resource))
+
+        try:
+            message_id = self._request_graph.objects(request_node, CURATOR.messageId, card=1)
+        except ValueError as e:
+            raise SyntaxError('Invalid number of message identifiers: {}'.format(e.message))
+        log.debug('Message identified as {}'.format(message_id))
+
+        try:
+            submitted_on = self._request_graph.objects(request_node, CURATOR.submittedOn, card=1)
+        except ValueError as e:
+            raise SyntaxError('Invalid number of submission dates: {}'.format(e.message))
+        log.debug('Message submitted on {}'.format(submitted_on))
+
+        try:
+            submitter_node = self._request_graph.objects(request_node, CURATOR.submittedBy, card=1)
+            submitted_by = self._request_graph.objects(submitter_node, CURATOR.agentId, card=1)
+        except ValueError as e:
+            raise SyntaxError('Invalid number of submitters: {}'.format(e.message))
+        log.debug('Message submitted by {}'.format(submitted_by))
+
+        dh_res = self._request_graph.query("""SELECT ?ex ?q ?rk ?h ?p ?v WHERE {
+                                              [] a curator:EnrichmentRequest;
+                                                 curator:replyTo [
+                                                    a curator:DeliveryChannel;
+                                                    amqp:exchangeName ?ex;
+                                                    amqp:queueName ?q;
+                                                    amqp:routingKey ?rk;
+                                                    amqp:broker [
+                                                       a amqp:Broker;
+                                                       amqp:host ?h;
+                                                       amqp:port ?p;
+                                                       amqp:virtualHost ?v
+                                                    ]
+                                                 ]
+                                              } """)
+        dh_res = list(dh_res)
+        if len(dh_res) != 1:
+            raise SyntaxError('Invalid delivery channel specification')
+
+        (exchange_name, queue_name, routing_key, host, port, virtual_host) = dh_res.pop()
+        log.debug('Delivery change to reply to: {}')
