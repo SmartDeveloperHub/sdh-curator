2823feaa34f13446ba37cdc216a705510e5859a5
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2015-10-26 17:02:17 +0100

    Enabled sleepycat graph store

--- sdh/curator/server/config.py
+++ sdh/curator/server/config.py
@@ -46,7 +46,7 @@ class DevelopmentConfig(Config):
         'db': 4,
         'port': 6379
     }
-    STORE = 'memory'
+    STORE = 'persist'
     AGORA = 'http://localhost:9001'
 
 
--- sdh/curator/store/triples.py
+++ sdh/curator/store/triples.py
@@ -23,8 +23,23 @@
 """
 
 from rdflib import ConjunctiveGraph
+from sdh.curator.server import app
+import logging
 
 __author__ = 'Fernando Serena'
 
-graph = ConjunctiveGraph()
+log = logging.getLogger('sdh.curator.store.triples')
+
+store_mode = app.config['STORE']
+if 'persist' in store_mode:
+    log.info('Loading known triples...')
+    graph = ConjunctiveGraph('Sleepycat')
+    graph.open('graph_store', create=True)
+else:
+    graph = ConjunctiveGraph()
+
+
+graph.store.graph_aware = False
+# log.debug('\n{}'.format(graph.serialize(format='turtle')))
+log.info('Ready')
 
